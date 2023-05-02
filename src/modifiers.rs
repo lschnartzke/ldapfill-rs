e5f47bab6b7e0d7705e516c9d366861580edcb6c
Leon Schnartzke <lschnartzke@gmail.com>
2023-05-02 14:36:33 +0200

    implement loading the file cache

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -4,6 +4,8 @@ mod types;
 
 use file_cache::FileCache;
 use parser::Modifier as ModifierKind;
+use std::{path::PathBuf, str::FromStr};
+
 pub use types::*;
 
 use self::parser::Token;
@@ -24,13 +26,52 @@ impl ModifierTree {
         }
     }
 
-    /// Traverses the ModifierTree for `Modifier::File`s and builds a file cache to use 
+    /// Traverses the ModifierTree for `Modifier::File`s and builds a file cache to use
     /// for more efficient value generation.
-    pub async fn build_file_cache(&self) -> FileCache {
-        let mut files = Vec::new();
-        
+    pub(crate) async fn build_file_cache(&self) -> FileCache {
+        let args = self.collect_file_arguments();
 
+        let mut cache = FileCache::new();
 
+        for arg in args {
+            cache
+                .load_file(PathBuf::from(arg))
+                .await
+                .expect("To open file")
+        }
+
+        cache
+    }
+
+    /// Collects all arguments to `ModifierKind::File`s. Panics if the
+    /// argument is not a string.
+    fn collect_file_arguments<'s>(&'s self) -> Vec<&'s str> {
+        let mut res = vec![];
+
+        match *self {
+            Token::String(_) => (),
+            Token::Modifier(modifier, ref args) => match modifier {
+                ModifierKind::File if args.len() == 1 => {
+                    if let Token::String(s) = &args[0] {
+                        res.push(s.as_str())
+                    } else {
+                        panic!("ModifierKind::File only accepts String arguments, got {args:#?} instead")
+                    }
+                }
+                ModifierKind::Uppercase | ModifierKind::Combine | ModifierKind::Lowercase => res
+                    .extend(
+                        args.iter()
+                            .flat_map(ModifierTree::collect_file_arguments)
+                            .collect::<Vec<&str>>(),
+                    ),
+
+                ModifierKind::File => {
+                    panic!("`ModifierKind::File` expects exactly one string argument")
+                }
+            },
+        }
+
+        res
     }
 
     fn apply_modifier(&self, modifier: ModifierKind, args: &Vec<ModifierTree>) -> String {
@@ -58,10 +99,12 @@ impl ModifierTree {
                 .join(""),
             // TODO: Load random string from file cache.
             ModifierKind::File if args.len() == 1 => unimplemented!(),
-            _ => panic!("invalid number of arguments for {modifier:?}: {}", args.len()),
+            _ => panic!(
+                "invalid number of arguments for {modifier:?}: {}",
+                args.len()
+            ),
         }
     }
-
 }
 
 #[cfg(test)]
@@ -109,4 +152,94 @@ mod test {
 
         assert_eq!("Hello, world!", modifier_tree.apply().as_str())
     }
+
+    #[test]
+    fn collect_file_modifier_arguments_simple_string() {
+        let tree = Token::Modifier(
+            ModifierKind::File,
+            vec![Token::String(String::from("lastname.txt"))],
+        );
+
+        let args = tree.collect_file_arguments();
+
+        assert_eq!(args, vec!["lastname.txt"]);
+    }
+
+    #[test]
+    fn collect_file_modifier_arguments_nested_tree() {
+        let tree = Token::Modifier(
+            ModifierKind::Combine,
+            vec![
+                Token::Modifier(
+                    ModifierKind::File,
+                    vec![Token::String(String::from("firstname.txt"))],
+                ),
+                Token::String(String::from(" ")),
+                Token::Modifier(
+                    ModifierKind::File,
+                    vec![Token::String(String::from("lastname.txt"))],
+                ),
+            ],
+        );
+
+        let args = tree.collect_file_arguments();
+
+        assert_eq!(args, vec!["firstname.txt", "lastname.txt"]);
+    }
+
+    #[test]
+    fn collect_file_modifier_arguments_deepish_nested() {
+        let tree = Token::Modifier(
+            ModifierKind::Combine,
+            vec![Token::Modifier(
+                ModifierKind::Lowercase,
+                vec![Token::Modifier(
+                    ModifierKind::File,
+                    vec![Token::String(String::from("firstname.txt"))],
+                )],
+            ),
+            Token::String(String::from(".")),
+            Token::Modifier(ModifierKind::Lowercase,
+                            vec![Token::Modifier(ModifierKind::File, vec![Token::String(String::from("lastname.txt"))])]),
+            Token::String(String::from("@")),
+            Token::Modifier(ModifierKind::Lowercase, vec![
+                            Token::Modifier(ModifierKind::File, vec![Token::String(String::from("company.txt"))])
+            ]),
+            Token::String(String::from(".com"))
+            ],
+        );
+
+        let args = tree.collect_file_arguments();
+
+        assert_eq!(args, vec!["firstname.txt", "lastname.txt", "company.txt"]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn collect_file_modifier_arguments_invalid_count() {
+        let tree = Token::Modifier(ModifierKind::File, vec![Token::String(String::from("Hello")), Token::String(String::from(", world!"))]);
+
+
+        // panic 
+        tree.collect_file_arguments();
+    }
+
+    #[test]
+    #[should_panic]
+    fn collect_file_modifier_arguments_invalid_type() {
+        let tree = Token::Modifier(ModifierKind::File, vec![Token::Modifier(ModifierKind::Uppercase, vec![Token::String(String::from("hello.txt"))])]);
+
+        // panic
+        tree.collect_file_arguments();
+    }
+
+    #[test]
+    fn collect_file_modifier_arguments_empty_list() {
+        let tree = Token::Modifier(ModifierKind::Uppercase, vec![Token::String(String::from("lastname"))]);
+
+        let args = tree.collect_file_arguments();
+
+        assert_eq!(args, Vec::<&str>::new());
+    }
+
 }
