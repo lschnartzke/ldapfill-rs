09224b1f05f944a22cbe52966d0490adcc5a64d6
Leon Schnartzke <lschnartzke@gmail.com>
2023-05-15 20:18:57 +0200

    add average entries per second to progress bar

--- src/progress.rs
+++ src/progress.rs
@@ -1,8 +1,9 @@
 /// Provides a progress bar that shows how many entries have been created. 
 
 use tokio::sync::mpsc::{UnboundedSender, UnboundedReceiver, unbounded_channel};
+use tokio::time;
 use tokio_stream::{wrappers::UnboundedReceiverStream, StreamExt};
-use indicatif::ProgressBar;
+use indicatif::{ProgressBar, ProgressStyle};
 
 // TODO: Transport messages via the channel. This allows for smoother display and 
 // avoid weird left-overs when the bar moves down a line bcause we printed 
@@ -19,12 +20,35 @@ pub async fn start_progress_task(max_count: u64) -> ProgressSender {
 }
 
 async fn progress_task(max_count: u64, rx: ProgressReceiver) {
+    let style = ProgressStyle::with_template("{wide_bar} [{pos}/{len}] {msg}").expect("valid style");
     let bar = ProgressBar::new(max_count);
+    bar.set_style(style);
     let mut stream = UnboundedReceiverStream::new(rx);
+    let mut count = 0;
+    let start = time::Instant::now();
+    let mut current_interval = start;
+    let mut current_count = 0;
 
     while let Some(_) = stream.next().await {
         bar.inc(1);
+        count += 1;
+        current_count += 1;
+
+        let now = time::Instant::now();
+        if (now - current_interval).as_secs() >= 1 {
+            let msg = format!("{current_count} entries/second");
+            bar.set_message(msg.clone());
+            current_count = 0;
+            current_interval = now;
+        }
+
     }
 
-    bar.finish();
+    let end = time::Instant::now();
+    let total_duration = end-start;
+    let avg = count/total_duration.as_secs();
+    let msg = format!("Created {avg} entries/second on average");
+    
+    bar.finish_with_message(msg);
+
 }
